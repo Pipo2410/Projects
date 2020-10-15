7d7776cf212c0e4a4ef8b922fca639df8f34aaf4
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2020-10-16 01:07:24 +0200

    React Natours with autosuggest Searchbar

---
+++ reactNatours/my-app/build/precache-manifest.9ccadee10456da242f45d6e90a80a6b7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72a0a14865352ca18ee29b74b418dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3fb3889464957fba895",
+    "url": "/static/css/main.643f0ce8.chunk.css"
+  },
+  {
+    "revision": "d35a4b300f625dfd935d",
+    "url": "/static/js/2.3bf9a27f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3bf9a27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fb3889464957fba895",
+    "url": "/static/js/main.ed849e19.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/static/media/logoWhite.40d4ef82.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/static/media/nat-7.5f846d16.jpg"
+  }
+]);
